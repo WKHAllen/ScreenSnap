10a6228f54c7cb37a5cb7e1f74534def45ad8dcb
WKHAllen <wkhallen@gmail.com>
2020-04-10 20:28:14 -0400

    Set up keyboard hook

---
+++ ScreenSnap/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSnap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InterceptKeys.Start(OnKeyPress);
+        }
+
+        static void OnKeyPress(int keycode)
+        {
+            Console.WriteLine(keycode);
+            Console.WriteLine((Keys)keycode);
+            Console.WriteLine((Keys)keycode == Keys.PrintScreen);
+        }
+    }
+}
