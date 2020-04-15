5e99da92095c8756484168b8d9330b24ce4bacd9
WKHAllen <wkhallen@gmail.com>
2020-04-15 17:07:42 -0400

    Build UI save folder browsing options

--- ScreenSnapUI/MainForm.Designer.cs
+++ ScreenSnapUI/MainForm.Designer.cs
@@ -28,21 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.SaveFolderDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.SaveFolderButton = new System.Windows.Forms.Button();
+            this.SaveFolderLabel = new System.Windows.Forms.Label();
+            this.SaveFolderBox = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // SaveFolderDialog
+            // 
+            this.SaveFolderDialog.Description = "Select save folder";
+            // 
+            // SaveFolderButton
+            // 
+            this.SaveFolderButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveFolderButton.Location = new System.Drawing.Point(713, 12);
+            this.SaveFolderButton.Name = "SaveFolderButton";
+            this.SaveFolderButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveFolderButton.TabIndex = 0;
+            this.SaveFolderButton.Text = "Browse";
+            this.SaveFolderButton.UseVisualStyleBackColor = true;
+            this.SaveFolderButton.Click += new System.EventHandler(this.SaveFolderButton_Click);
+            // 
+            // SaveFolderLabel
+            // 
+            this.SaveFolderLabel.AutoSize = true;
+            this.SaveFolderLabel.Location = new System.Drawing.Point(12, 15);
+            this.SaveFolderLabel.Name = "SaveFolderLabel";
+            this.SaveFolderLabel.Size = new System.Drawing.Size(84, 17);
+            this.SaveFolderLabel.TabIndex = 1;
+            this.SaveFolderLabel.Text = "Save folder:";
+            // 
+            // SaveFolderBox
+            // 
+            this.SaveFolderBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveFolderBox.Location = new System.Drawing.Point(102, 12);
+            this.SaveFolderBox.Name = "SaveFolderBox";
+            this.SaveFolderBox.Size = new System.Drawing.Size(605, 22);
+            this.SaveFolderBox.TabIndex = 2;
+            this.SaveFolderBox.TextChanged += new System.EventHandler(this.SaveFolderBox_Changed);
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SaveFolderBox);
+            this.Controls.Add(this.SaveFolderLabel);
+            this.Controls.Add(this.SaveFolderButton);
             this.Name = "MainForm";
             this.Text = "ScreenSnap";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
 
         #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog SaveFolderDialog;
+        private System.Windows.Forms.Button SaveFolderButton;
+        private System.Windows.Forms.Label SaveFolderLabel;
+        private System.Windows.Forms.TextBox SaveFolderBox;
     }
 }
-
--- ScreenSnapUI/MainForm.cs
+++ ScreenSnapUI/MainForm.cs
@@ -1,25 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace ScreenSnapUI
 {
     public partial class MainForm : Form
     {
+        private static string savePath = "images";
+        private const string iniPath = "Config.ini";
+        private static readonly FileIniDataParser iniParser = new FileIniDataParser();
+        private static IniData iniData = new IniData();
+
         public MainForm()
         {
             InitializeComponent();
+            CreateIni();
+            UpdateSavePath();
+        }
+
+        private static void CreateIni()
+        {
+            if (!File.Exists(iniPath))
+            {
+                iniData["Config"]["SavePath"] = "images";
+                iniParser.WriteFile(iniPath, iniData);
+            }
+        }
+
+        private static void UpdateSavePath()
+        {
+            iniData = iniParser.ReadFile(iniPath);
+            savePath = iniData["Config"]["SavePath"].ToString();
+            Directory.CreateDirectory(savePath);
         }
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            SaveFolderBox.Text = iniData["Config"]["SavePath"].ToString();
+        }
 
+        private void SaveFolderButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = SaveFolderDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                SaveFolderBox.Text = SaveFolderDialog.SelectedPath;
+                iniData["Config"]["SavePath"] = SaveFolderDialog.SelectedPath;
+                iniParser.WriteFile(iniPath, iniData);
+                Invalidate();
+            }
+        }
+
+        private void SaveFolderBox_Changed(object sender, EventArgs e)
+        {
+            iniData["Config"]["SavePath"] = SaveFolderBox.Text;
+            iniParser.WriteFile(iniPath, iniData);
         }
     }
 }
